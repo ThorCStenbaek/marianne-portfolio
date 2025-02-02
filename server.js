25948a0b3331397cc6d1fc9a5fef8a3d1d0afc94
Thor Christian Stenbaek <stenbaekt@gmail.com>
2025-02-02 18:16:35 +0100

    changes for marianen

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000; 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const path = require('path');
